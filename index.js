4a5bccb91ddf12b93be19f300be1b2fc033f4b38
goliatone <burgosemi@gmail.com>
2017-02-21 15:01:06 -0500

    adding strings;

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 /*jshint esversion:6, node:true*/
 'use strict';
 
+/**
+ * Module initializer.
+ */
 module.exports.init = require('./lib/init');
 
+/**
+ * Utils
+ */
 module.exports.utils = require('./lib/utils');
 
+/**
+ * Class helps with Setup.
+ */
 module.exports.Setup = require('./lib/setup');
 
+/**
+ * Server Class
+ */
 module.exports.Server = require('./lib/server');
-
+/**
+ * Configurator class.
+ */
 module.exports.Configurator = require('./lib/configurator');
-
+/**
+ * initializer for express sub apps.
+ */
 module.exports.initializeSubapp = require('./lib/initializeSubapp');
-
+/**
+ * createServer
+ * @method createServer
+ * @param  {Object}     config
+ * @return {Object}     Server instance.
+ */
 module.exports.createServer = function(config){
     let Server = module.exports.Server;
     var instance = new Server(config);
