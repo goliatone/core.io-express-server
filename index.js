88ed50fbf5c455744a1e43b4f0c0abe7c585ae79
goliatone <burgosemi@gmail.com>
2017-02-21 13:43:29 -0500

    refactoring main file

--- index.js
+++ index.js
@@ -1,118 +1,14 @@
 /*jshint esversion:6, node:true*/
 'use strict';
 
-const express = require('express');
-const EventEmitter = require('events');
-const path = require('path');
-/**
- * Module dependencies.
- */
-const http = require('http');
-const Setup = require('./setup');
-const getPort = require('./utils').getPort;
+module.exports.init = require('./lib/init');
+module.exports.utils = require('./lib/utils');
+module.exports.Setup = require('./lib/setup');
+module.exports.Server = require('./lib/server');
+module.exports.Configurator = require('./lib/configurator');
 
-/**
- * Listen on provided port, on all network interfaces.
- */
-class Server extends EventEmitter {
-
-    constructor(config){
-        super();
-        this.logger = config.logger || console;
-        this.config = config;
-    }
-
-    start(opt){
-        let app = express();
-
-        var setup = new Setup(app, this.config);
-        setup.logger = this.logger;
-
-        this.express = setup.express;
-
-        setup.addMiddleware();
-        
-        setup.addViews();
-
-        // this.once('server.routes', setup.addRoutes.bind(setup));
-        // this.once('server.error-handlers', setup.addErrorHandlers.bind(setup));
-        setup.mount();
-
-        setup.addErrorHandlers();
-
-        this.emit('server.pre-routes');
-        this.emit('server.routes');
-        this.emit('server.post-routes');
-
-        this.emit('server.pre-error-handlers');
-        this.emit('server.error-handlers');
-        this.emit('server.post-error-handlers');
-
-        /**
-         * Get port from environment and store in Express.
-         */
-        const port = getPort();
-        this.logger.info('listening on port %s', port);
-        this.server = setup.createServer(port);
-
-        this.server.on('error', this.onError.bind(this));
-        this.server.on('listening', this.onReady.bind(this));
-
-        return this;
-    }
-
-    get transport(){ return this.server; }
-
-    get serverApp(){ return this.express; }
-
-    /**
-     * Event listener for HTTP server "listening" event.
-     */
-    onReady(){
-
-        var addr = this.server.address();
-        var bind = typeof addr === 'string'
-            ? 'pipe ' + addr
-            : 'port ' + addr.port;
-
-        this.logger.info('Listening on ' + bind);
-
-        this.emit('server.ready', this);
-    }
-
-    /**
-     * Event listener for HTTP server "error" event.
-     */
-    onError(error){
-        if (error.syscall !== 'listen') {
-            throw error;
-        }
-
-        var bind = typeof port === 'string'
-            ? 'Pipe ' + port
-            : 'Port ' + port;
-
-        // handle specific listen errors with friendly messages
-        switch (error.code) {
-            case 'EACCES':
-                console.error(bind + ' requires elevated privileges');
-                process.exit(1);
-                break;
-            case 'EADDRINUSE':
-                console.error(bind + ' is already in use');
-                process.exit(1);
-                break;
-            default:
-                throw error;
-        }
-    }
-}
-
-module.exports = Server;
 module.exports.createServer = function(config){
+    let Server = module.exports.Server;
     var instance = new Server(config);
     return instance;
 };
-
-
-module.exports.Configurator = require('./configurator');
