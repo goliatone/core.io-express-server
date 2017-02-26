cf3acc5c098e1a27506594ca71e4506fee05ef57
goliatone <burgosemi@gmail.com>
2017-02-26 17:00:37 -0500

    example: wip

--- example/myapp/routes/home.js
+++ example/myapp/routes/home.js
@@ -3,12 +3,20 @@
 
 var express = require('express');
 var router = express.Router();
-
+var applyPolicies = require('../middleware/core.io-auth').applyPolicies;
 
 module.exports = function(app, config){
-    router.get('/hello', (req, res)=>{
-        console.log('HERE!!');
-        res.status(200).send('OK');
+
+    let policies = applyPolicies('GET /hello', app, config);
+
+    router.get('/hello', policies, (req, res)=>{
+        console.log('/hello');
+        res.status(200).send('<b>Hello</b>');
+    });
+
+    router.get('/testing', [], (req, res)=>{
+        console.log('/testing');
+        res.status(200).send('<b>Testing</b>');
     });
 
     app.use('/', router);
