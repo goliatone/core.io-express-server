816db0e5a8ba2e629fd894fe99f1e8270a4c56d1
goliatone <burgosemi@gmail.com>
2017-02-26 16:58:32 -0500

    wip

--- example/myapp/middleware/core.io-auth/index.js
+++ example/myapp/middleware/core.io-auth/index.js
@@ -4,7 +4,7 @@ const extend = require('gextend');
 const express = require('express');
 const Keypath = require('gkeypath');
 const passport = require('passport');
-
+const crypto = require('./cryptoUtils');
 
 module.exports = function(app, config){
     console.log('--- passport');
@@ -27,10 +27,6 @@ module.exports = function(app, config){
     });
 
     /*
-     * Use all declared strategies
-     */
-
-    /*
      * Create all default routes so we can handle
      * authentication flow:
      * - login
@@ -48,20 +44,70 @@ module.exports = function(app, config){
     //TODO: config.routes.logout;
     router.get('/logout', function(req, res){
         req.logout();
-
-        req.session.authenticated = false;
-
         req.session.destroy(function(err) {
             // cannot access session here
             res.redirect('/');
         });
     });
 
+    /// THIS SHOULD BE OPTIONAL ////////////
     router.get('/signup', function(req, res){
         res.render('signup', locals);
     });
 
+    router.post('/signup', function(req, res){
+        res.render('signup', locals);
+
+        /*config.createUser(req.body).then((user)=>{
+            req.redirect('/login');
+        }).catch((err)=>{
+            locals.user = req.body;
+            res.render('signup', locals);
+        });*/
+    });
+    //////////////////////////////////////////
+    /// LOAD STRATEGIES
+    //////////////////////////////////////////
+    app.post('/login', (req, res, next) => {
+        passport.authenticate('local', (err, user, params) => {
+            if (err) return next(err);
+            if (!user) return res.status(401).json({error: params ? params.message : 'Invalid login'});
+            req.login(user, {}, error => {
+                if (error) return res.status(500).json({error: error.message});
+                res.json({
+                    user: user,
+                    returnTo: (req.session && req.session.returnTo) ? req.session.returnTo : '/'
+                });
+                return null;
+            });
+        })(req, res, next);
+    });
+
+    let LocalStrategy = require('passport-local');
+    passport.use(new LocalStrategy({
+            usernameField: 'email'
+        },(id, password, done) => {
+        console.log('local strategy: id %s password %s', id, password);
+        let authUser;
+        return findUserById(id).then((user) => {
+            console.log('findUserById:', id, user);
+            if (!user) return false;
+            authUser = user;
+            return crypto.compare(user, password);
+        }).then(isMatch => {
+            if (isMatch) done(null, authUser);
+            else done(null, false, {message: 'Incorrect email or password'});
+            return null;
+        })
+        .catch(done);
+    }));
+
+    /*
+     * Use all declared strategies
+     */
     app.use('/', router);
+
+
 
     /*
      * Initialize session passport
@@ -72,3 +118,5 @@ module.exports = function(app, config){
 
     app.use(passport.session());
 };
+
+module.exports.applyPolicies = require('./applyPolicies');
