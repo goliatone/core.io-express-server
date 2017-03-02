3f1bc8eafe8d92a57a60d5e2dab0b1aeea3d60c8
goliatone <burgosemi@gmail.com>
2017-03-02 17:56:09 -0500

    wip core.io-auth

--- example/myapp/middleware/core.io-auth/index.js
+++ example/myapp/middleware/core.io-auth/index.js
@@ -8,18 +8,34 @@ const passport = require('passport');
 const crypto = require('./cryptoUtils');
 
 module.exports = function(app, config){
-    console.log('--- passport');
-    console.log(Object.keys(config));
+
+    if(!config.logger) config.logger = console;
+
+    config.logger.info('--- passport');
+    config.logger.info(Object.keys(config));
 
     /*
      * TODO: Normalize names. Should we match
      * Waterline methods or have desriptive
      * names?
+     * Make simple:
+     *
+     * getUser: function(){
+     *     return PassportUser;
+     * },
+     * getPassport: function(){
+     *     return Passport;
+     * }
      */
     let findOne = config.passport.model.findOne;
     let createUser = config.passport.model.createUser;
     let findUserById = config.passport.model.findUserById;
     let cleanUser = config.passport.model.cleanUser || _cleanUser;
+    let deleteUser = config.passport.model.deleteUser;
+    let updateUser = config.passport.model.updateUser;
+
+    let createPassport = config.passport.model.createPassport;
+    let findPassport = config.passport.mdoel.findPassport;
 
     /*
      * We need to provide passport with a
@@ -29,11 +45,118 @@ module.exports = function(app, config){
     passport.serializeUser((user, done) => done(null, user.id));
 
     passport.deserializeUser((id, done) => {
-        findUserById(id).then(user => {
+        return findUserById(id).then(user => {
             done(null, user);
-            return null;
+            return user;
         }).catch(done);
     });
+
+
+    passport.protocols = rquire('./protocols')(passport, config);
+
+    /*
+     * Extend passport object with an `endpoint`
+     * method to handle all 3rd party providers.
+     */
+    passport.endpoint = function(req, res){
+        let strategies = config.passport;
+        let provider = req.param('provider');
+        let options = {};
+
+        /*
+         * We did not define the provider.
+         */
+        if(!strategies.hasOwnProperty(provider)){
+            return res.redirect('/login');
+        }
+
+        ['scope', 'hd', 'display'].map(function(key){
+            if(!strategies[provider].options.hasOwnProperty(key)) return;
+            options[key] = strategies[provider].options[key];
+        });
+
+        this.authenticate(provider, options)(req, res, req.next);
+    };
+
+    /*
+     * Create an authentication callback endpoint.
+     */
+    passport.callback = function(req, res, next){
+        let action = req.param('action');
+        let provider = req.param('provider', 'local');
+
+        if(action === 'disconnect' && req.user){
+            return this.disconnect(req, res, next);
+        } else {
+            next(new Error('Invalid action'));
+        }
+
+        if(provider === 'local'){
+            if(action === 'register' && !req.user){
+                return this.protocols.local.register(req, res, next);
+            }
+
+            if(action === 'connect' && req.user){
+                return this.protocols.local.connect(req, res, next);
+            }
+
+            if(action === 'disconnect' && req.user){
+                return this.protocols.local.disconnect(req, res, next);
+            }
+
+        } else {
+            if( action === 'disconnect' && req.user ){
+                return this.disconnect(req, res, next);
+            }
+
+            this.authenticate(provider, next)(req, res, req.next);
+        }
+
+        next(new Error('Invalid action'));
+    };
+
+    pasport.connect = function(req, query, profile, next){
+        let user = {};
+
+        let provider = profile.provider || req.param('provider');
+
+        req.session.tokens = query.tokens;
+
+        query.provider = provider;
+
+        if(!provider){
+            return next(new Error('No authentication provider was found.'));
+        }
+
+        config.logger.info('auth profile', profile);
+
+        if(profile.emails && profile.emails[0]){
+            user.email = profile.emails[0].value;
+        }
+
+        if(profile.username){
+            user.username = profile.username;
+        }
+
+        if(!user.username && !user.email){
+            return next(new Error('Neither email or username was available'));
+        }
+    };
+
+    passport.disconnect = function(req, res, next){
+        let user = req.user;
+        let provider = req.param('provider');
+
+        return findOne({
+            provider: provider,
+            user: user.id
+        }).then((record)=>{
+            return deleteUser(record.id);
+        }).then(()=>{
+            next(null, user);
+            return user;
+        }).catch(next);
+    };
 
     /*
      * Initialize session passport
@@ -45,6 +168,130 @@ module.exports = function(app, config){
     app.use(passport.initialize());
 
     app.use(passport.session());
+
+    /*
+     * ROUTES:
+     * POST /register UserController.create
+     * POST /logout AuthController.logout
+     *
+     * POST /auth/local AuthController.callback
+     * POST /auth/local/:action AuthController.callback
+     *
+     * POST /auth/:provider AuthController.callback
+     * POST /auth/:provider/:action AuthController.callback
+     *
+     * GET /auth/:provider AuthController.provider
+     * GET /auth/:provider/callback AuthController.callback
+     * GET /auth/:provider/:action AuthController.callback
+     *
+     * GET /auth/google AuthController.provider
+     * GET /auth/google/callback AuthController.callback
+     * GET /auth/google/create AuthController.callback
+     */
+    let AuthController = {
+        logout: function(req, res){
+            /*
+             * logout should be handled only
+             * by POST methods.
+             */
+            if(req.method.toUpperCase() !== 'POST'){
+                return res.send(400);
+            }
+
+            req.logout();
+            delete req.user;
+            delete req.session.passport;
+            req.session.authenticated = false;
+            // req.session.destroy((err)=>{
+                res.redirect(req.query.next || '/');
+            // });
+        },
+        /*
+         * Creates a 3rd party authentication
+         * endpoint.
+         *
+         * i.e. GET /auth/google
+         *
+         * This basically calls `passport.authenticate`
+         * which would redirect user to the provider
+         * for authentication.
+         * On complete, the provider must redirect
+         * the user back to /auth/:provider/callback
+         */
+        provider: function(req, res){
+            //TODO: get passport from context?! Or pass
+            //when we create AuthController...
+            passport.endpoint(req, res);
+        },
+        /*
+         * Authentication callback endpoint.
+         * Handles creating and verifying `Passport`s
+         * and `PassportUser`s, both locally and 3rd
+         * party.
+         *
+         * It handles three actions:
+         * - register
+         * - login
+         * - disconnect
+         *
+         */
+        callback: function (req, res){
+            let action = req.param('action');
+
+            passport.callback(req, res, (err, user, info, status)=>{
+                if(err || !user){
+                    config.logger.warn(user, err, info, status);
+                    return _negotiateError(res, err || info, action);
+                }
+
+                req.login(user, (err)=>{
+                    if(err){
+                        config.logger.warn('AuthController callback', err);
+                        return _negotiateError(res, err, action);
+                    }
+
+                    req.session.authenticated = true;
+
+                    if(req.query.next){
+                        let url = _buildCallbacNextUrl(req);
+                        res.status(302).set('Location', url);
+                    }
+                    config.logger.info('User authenticated OK %s', user);
+                    return res.json(user);
+                });
+            });
+        },
+        /*
+         * Disconnect a passport from a user.
+         */
+        disconnect: function(req, res){
+            passport.disconnect(req, res);
+        }
+    };
+
+    function _negotiateError(res, err, action){
+        if(action === 'register' || action === 'login'){
+            return res.redirect('/' + action);
+        } else if(action === 'disconnect'){
+            return res.redirect('back');
+        }
+
+        res.send(403, err);
+    }
+
+    function _buildCallbacNextUrl(req){
+        const K = require('gkeypath');
+
+        let includeToken = req.query.includeToken;
+        let url = K.get(req, 'session.returnTo', req.query.next;
+        let accessToken = K.get(req, 'session.tokens.accessToken');
+
+        if(includeToken && accessToken){
+            return url + '?access_token=' + accessToken;
+        }
+
+        return url;
+    }
 
     /*
      * Create all default routes so we can handle
@@ -62,12 +309,7 @@ module.exports = function(app, config){
     });
 
     //TODO: config.routes.logout;
-    router.get('/logout', function(req, res){
-        req.logout();
-        req.session.destroy(function(err) {
-            res.redirect('/');
-        });
-    });
+    router.get('/logout', AuthController.logout);
 
     /// THIS SHOULD BE OPTIONAL ////////////
     router.get('/signup', function(req, res){
@@ -131,15 +373,15 @@ module.exports = function(app, config){
         // session: false
     };
 
-    passport.use(new LocalStrategy(strategyConfig,(req, id, password, done) => {
-        console.log('local strategy: id %s password %s', id, password);
+    passport.use(new LocalStrategy(strategyConfig,(req, identifier, password, done) => {
+        console.log('local strategy: id %s password %s', identifier, password);
 
         let authUser;
         let query = {};
-        query[strategyConfig.usernameField || 'username'] = id;
+        query[strategyConfig.usernameField || 'username'] = identifier;
 
         return findOne(query).then((user) => {
-            console.log('findOne:', id, user);
+            console.log('findOne:', identifier, user);
             if (!user) return false;
             authUser = user;
             return crypto.compare(user, password);
